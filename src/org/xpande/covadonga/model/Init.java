2a9cc6b7546d74a4f9d48d97be7d45e40bbc6bef
gvilauy <gabrielvila13@gmail.com>
2017-09-06 13:12:46 -0300

    Initial commit

---
+++ src/org/xpande/covadonga/model/Init.java
@@ -0,0 +1,9 @@
+package org.xpande.covadonga.model;
+
+/**
+ * Product: Adempiere ERP & CRM Smart Business Solution. Localization : Uruguay - Xpande
+ * Xpande. Created by Gabriel Vila on 9/6/17.
+ */
+public class Init {
+
+}
